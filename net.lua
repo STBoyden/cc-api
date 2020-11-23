467682d68fb52a4ffcc637c5a7a8accd1de0e264
STBoyden <stboyden@stboyden.xyz>
2020-11-23 22:13:59 +0000

    Reformat doc comments

--- net.lua
+++ net.lua
@@ -1,10 +1,8 @@
 local net = {}
 
--- [[
 -- net.open_connection(
 --      port: number
 -- ) -> number | nil
--- ]]
 net.open_connection = function(port)
     if (port == nil) then
         return nil
@@ -26,11 +24,9 @@ net.open_connection = function(port)
     return port
 end
 
--- [[
 -- net.close_connection(
 --      port: number
 -- ) -> number | nil
---]]
 net.close_connection = function(port)
     if (port == nil) then
         return nil
@@ -52,20 +48,19 @@ net.close_connection = function(port)
     return port
 end
 
--- [[
 -- net.send_message(
 --      port: number
 --      destination_port: number
 --      message: any
 --      close_after_send: boolean
 -- ) -> nil
---]]
 net.send_message = function(port, destination_port, message, ...)
     local response = net.open_connection(port)
     local close_after_send = {...}
 
     if (response == nil) then
-        return nil
+        print("Couldn't send message")
+        return
     end
 
     local modem = peripheral.find("modem")
